bb284755298c623a9c111941e9d90d24fa852f8b
biozhangjn <biozhangjn@gmail.com>
2020-03-23 23:38:38 +0800

    Blog update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #cd d:OneDrive/git/MyBlog
-git config --global user.name "biozhangjn"
-git config --global user.email "biozhangjn@gmail.com"
+#git config --global user.name "biozhangjn"
+#git config --global user.email "biozhangjn@gmail.com"
 
 #清除之前的SSH设置，4需要管理员权限
 #git config --global -l
