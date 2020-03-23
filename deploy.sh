4518bacb98fa672199f6587599d157eca2517fb1
biozhangjn <biozhangjn@gmail.com>
2020-03-23 23:38:09 +0800

    Blog update

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,14 @@
 
 #cd d:OneDrive/git/MyBlog
 git config --global user.name "biozhangjn"
-
 git config --global user.email "biozhangjn@gmail.com"
+
+#清除之前的SSH设置，4需要管理员权限
+#git config --global -l
+#git config --system -l
+#git config --global --unset credential.helper
+#git config --system --unset credential.helper
+
 #建立本地仓库
 git init
 #关联到Github仓库
