14eec8273d5d62f775e4fbb47f9bf76827c9e8e9
biozhangjn <biozhangjn@gmail.com>
2020-03-28 01:13:36 +0800

    Blog update

--- hugo-blog.R
+++ hugo-blog.R
@@ -37,4 +37,12 @@ git push -u origin master
 
 
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-162031575-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-162031575-1');
+</script>
